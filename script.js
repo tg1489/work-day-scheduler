1bb0396c0ea5717d2aa9f981a70a84bdd24ca7a5
Tony <tonyguarino1489@gmail.com>
2023-08-01 14:55:52 -0400

    Fixed localStorage not displaying data

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-
 $(function () {
-  $('button').click(function(e) {
+  $('button').click(function (e) {
     let textEl = $(this).prev(); // Gets the textarea element
     let thisValue = textEl.val(); // Gets the textarea value
-    if (thisValue==='') return; // If the textarea is empty, don't save anything.
+    if (thisValue === '') return; // If the textarea is empty, don't save anything.
     let parentID = $(this.parentNode).attr('id'); //Gets parent id of button (The Div)
     let thisKey = parentID;
-    localStorage.setItem(thisKey, thisValue); // Saves to local storage     
-})
+    localStorage.setItem(thisKey, thisValue); // Saves to local storage
+  });
 
   let currentTime = dayjs().format('H');
+  console.log(`currentTime: ${currentTime}`);
   let numbers = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
   // Use numbers to match with id and choose what class to add from there with ternary operator
-  numbers.map((e)=> {
-    let addClass = e == currentTime?'present':(e < currentTime?'past':'future');
-    $(`#hour-${e}`).addClass(addClass)
- 
-  //while updating the class, we can check if there is a localStorage with 
-  //the key that matches our id and add that into the textarea.
-  if (localStorage.getItem(`#hour-${e}`)) {
-    $(`#hour-${e}>textarea`).val(localStorage.getItem(`#hour-${e}`))
-  }
-  
-})
+  numbers.map((e) => {
+    console.log(`e: ${e}`);
+    let addClass =
+      e == currentTime ? 'present' : e < currentTime ? 'past' : 'future';
+    $(`#hour-${e}`).addClass(addClass);
 
+    //while updating the class, we can check if there is a localStorage with
+    //the key that matches our id and add that into the textarea.
+    if (localStorage.getItem(`hour-${e}`)) {
+      $(`#hour-${e}>textarea`).val(localStorage.getItem(`hour-${e}`));
+    }
+  });
 
-  $('#currentDay').html(dayjs().format('MMMM DD, YYYY H:mm'))
+  $('#currentDay').html(dayjs().format('MMMM DD, YYYY H:mm'));
 });
